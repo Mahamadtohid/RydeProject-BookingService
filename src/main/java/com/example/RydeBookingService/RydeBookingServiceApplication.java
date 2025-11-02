2f932663f249181cc488938bec2ff6965fad45a0
Tohid Naikwadi <naikwadimdtohid@gmail.com>
2025-11-02 13:18:46 +0530

    Setting up configuration and yml file

---
+++ src/main/java/com/example/RydeBookingService/RydeBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.RydeBookingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RydeBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RydeBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RydeBookingService/RydeBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RydeBookingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RydeBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
